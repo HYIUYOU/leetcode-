955c76c75d41a5a1bbb1a59d91c4b8158c44e719
HYIUYOU <52117880+HYIUYOU@users.noreply.github.com>
2020-10-15 18:09:50 +0800

    first commit

---
+++ Desktop/2-way_merge/cmake-build-debug/CMakeFiles/2_way_merge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2_way_merge"
+  "2_way_merge.pdb"
+  "CMakeFiles/2_way_merge.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_way_merge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
